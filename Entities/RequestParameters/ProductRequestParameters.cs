5e8755c8e285448a56a891320516ef98f9625c73
Alihan SARI <alihansari98@hotmail.com>
2024-10-29 21:37:55 +0300

    Product FilteredByPrice Method Added

--- Entities/RequestParameters/ProductRequestParameters.cs
+++ Entities/RequestParameters/ProductRequestParameters.cs
@@ -3,5 +3,8 @@ namespace Entities.RequestParameters
     public class ProductRequestParameters : RequestParameters
     {
         public int? CategoryId { get; set; }
+        public decimal MinPrice { get; set; } = 0;
+        public decimal MaxPrice { get; set; } = 9999999999999999.99m;
+        public bool IsValidPrice => MaxPrice > MinPrice;
     }
 }
